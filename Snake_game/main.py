a1f190a04a1db05c5634c1f69aa7733814f04ae5
Miron Mihai <98386090+mihaimiron1@users.noreply.github.com>
2025-06-25 20:11:36 +0300

    Update main.py

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -31,20 +31,20 @@ while gama_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        score.update_score()
+        score.increase_high_score()
 
     #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        gama_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
+        
 
     #Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            gama_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
-    
 
         
          
